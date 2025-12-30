848c0d8fb39c6a035bf83a0af5b0321b59459f70
ishikagupta21 <itz.ishu2105@gmail.com>
2025-12-30 14:22:48 +0530

    Added History Controller

--- src/main/java/com/vbs/demo/controller/HistoryController.java
+++ src/main/java/com/vbs/demo/controller/HistoryController.java
@@ -1,5 +1,6 @@
 package com.vbs.demo.controller;
 import com.vbs.demo.modals.History;
+import com.vbs.demo.repositories.HistoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
